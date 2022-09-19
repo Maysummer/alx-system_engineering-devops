5ef6a0e0f51604a4b57967aff6ea73239e8ac503
Maysummer <udojyk@gmail.com>
2022-09-18 17:55:10 -0700

    configure with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,22 @@
 # make changes to the config file
-
-file { '/etc/ssh/ssh_config':
-  ensure => present,
-  content => "Host *
-  	IdentifyFIle ~/.ssh/school
-	PasswordAuthentication no",
+include stdlib
+file_line { 'Host name':
+  ensure  => present,
+  path    => '~/.ssh/config',
+  line    => 'Host *',
+  replace => true,
+}
+-> file_line { 'identify':
+  ensure  => present,
+  path    => '~/.ssh/config',
+  line    => '	IdentityFile ~/.ssh/school',
+  after   => '^Host name',
+  replace => true,
+}
+-> file_line { 'password':
+  ensure  => present,
+  path    => '~/.ssh/config',
+  line    => '	PasswordAuthentication no',
+  after   => '^identify',
+  replace => true,
 }
